f328553413cfb311bb767c3265422385e59e14e7
Manuel Zarat <manuel@zarat.ml>
2025-06-01 01:40:15 +0200

    Create test-ocsp.sh

---
+++ test-ocsp.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+ 
+set -e
+ 
+CA_DIR="$HOME/myCA"
+SERVER=$1  
+SERVER_CERT="$CA_DIR/issued/$SERVER/$SERVER.cert.pem"
+ 
+URI=$(openssl x509 -in "$SERVER_CERT" -noout -ocsp_uri)
+ 
+openssl ocsp \
+  -issuer "$CA_DIR/certs/ca.cert.pem" \
+  -cert "$SERVER_CERT" \
+  -url $URI \
+  -resp_text -noverify
